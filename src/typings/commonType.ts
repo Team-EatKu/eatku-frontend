57357a76e51b3dc04ff365d6f478c2c17e897d35
taeseung-yoo <remicon99@gmail.com>
2024-02-29 01:57:46 +0900

    [FIX] Query 오류수정 any -> never

--- src/typings/commonType.ts
+++ src/typings/commonType.ts
@@ -2,4 +2,4 @@ import { SerializedStyles } from "@emotion/react";
 
 export interface CssType extends Record<string, string | SerializedStyles> {}
 
-export interface ParamsType extends Record<string | number, any> {}
+export interface ParamsType extends Record<string | number, never> {}
